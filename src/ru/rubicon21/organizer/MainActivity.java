2413ed24a98383eaa5971653809e006c0c6901c8
roman-nevr <roman-nevr@yandex.ru>
2015-03-18 22:46:25 +0300

    first

---
+++ src/ru/rubicon21/organizer/MainActivity.java
@@ -0,0 +1,73 @@
+package ru.rubicon21.organizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    ListView lvMain;
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        lvMain = (ListView) findViewById(R.id.lvMain);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.names, android.R.layout.simple_list_item_1);
+        lvMain.setAdapter(adapter);
+
+        //обработка нажатия на элемент списка
+        lvMain.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
+                        + id);
+            }
+        });
+        //обработка выделения (хз как использовать, но есть такая)
+        lvMain.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.d(LOG_TAG, "itemSelect: position = " + position + ", id = "
+                        + id);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d(LOG_TAG, "itemSelect: nothing");
+            }
+        });
+
+        //обработка прокрутки списка
+        lvMain.setOnScrollListener(new AbsListView.OnScrollListener() {
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                //обработка состояния прокрутки
+                //0 - стоит
+                //1 - его тащат
+                //2 - он листается сам
+                //еще немного
+                Log.d(LOG_TAG, "scrollState = " + scrollState);
+            }
+
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+                //обработка положения листа
+                /*Log.d(LOG_TAG, "scroll: firstVisibleItem = " + firstVisibleItem
+                        + ", visibleItemCount" + visibleItemCount
+                        + ", totalItemCount" + totalItemCount);*/
+            }
+        });
+
+    }
+}

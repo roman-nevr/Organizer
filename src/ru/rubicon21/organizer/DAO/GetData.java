130089bf6fa66d10564321fde09dd2b12674b0f6
Roman Berendeev <roman-nevr@yandex.ru>
2015-03-23 16:41:46 +0400

    вечерний коммит

--- src/ru/rubicon21/organizer/DAO/GetData.java
+++ src/ru/rubicon21/organizer/DAO/GetData.java
@@ -15,4 +15,85 @@ public class GetData {
         tasks.add(new Task("Тест 2","Вторая Пробная запись"));
         return tasks;
     }
+
+    /*
+     PK
+    |id|parent_id|name|description|has_children|
+
+    */
+    /*
+    * switch (v.getId()) {
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                // подготовим данные для вставки в виде пар: наименование столбца - значение
+
+                cv.put("name", name);
+                cv.put("email", email);
+                // вставляем запись и получаем ее ID
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+                break;
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+                // делаем запрос всех данных из таблицы mytable, получаем Cursor
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+
+                // ставим позицию курсора на первую строку выборки
+                // если в выборке нет строк, вернется false
+                if (c.moveToFirst()) {
+
+                    // определяем номера столбцов по имени в выборке
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+
+                    do {
+                        // получаем значения по номерам столбцов и пишем все в лог
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) +
+                                        ", name = " + c.getString(nameColIndex) +
+                                        ", email = " + c.getString(emailColIndex));
+                        // переход на следующую строку
+                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
+                break;
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                // удаляем все записи
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+        }
+        // закрываем подключение к БД
+        dbHelper.close();
+    }
+
+
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+            // создаем таблицу с полями
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "email text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+    * */
 }
--- src/ru/rubicon21/organizer/TaskDetails.java
+++ src/ru/rubicon21/organizer/TaskDetails.java
@@ -1,15 +1,54 @@
 package ru.rubicon21.organizer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import ru.rubicon21.organizer.DAO.GetData;
+import ru.rubicon21.organizer.adapter.MainWindowAdapter;
+import ru.rubicon21.organizer.entity.Task;
+
+import java.util.ArrayList;
 
 /**
  * Created by roma on 22.03.2015.
  */
 public class TaskDetails extends Activity {
+    final String LOG_TAG = "myLogs";
+    ListView lvMain;
+
+    MainWindowAdapter mainWindowAdapter;
+    ArrayList<Task> tasks;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.task_details);
+        setContentView(R.layout.main);
+
+        lvMain = (ListView) findViewById(R.id.lvMain);
+        tasks = new ArrayList<Task>();
+        tasks = (new GetData()).getTasks();
+        mainWindowAdapter = new MainWindowAdapter(this,tasks);
+
+        lvMain.setAdapter(mainWindowAdapter);
+
+        //обработка нажатия на элемент списка
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = " + id);
+
+                try {
+                    Intent intent = new Intent(TaskDetails.this, TaskDetails.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("NPE");
+                }
+            }
+        });
     }
 }
